f2071999dde9efe5095391789d442e8ebb78d60d
Alexandre Rocha <alexandrececchin@gmail.com>
2023-06-13 09:21:38 -0300

    [Red] Add new unit test when value is 3

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("fizzbuzz", () => {
     it('should return a value type of string',() => {
         expect(typeof fizzBuzz(2)).toBe('string')
     })
+
+    it('should return Fizz when the value is 3',() =>{
+        expect(fizzBuzz(3)).toBe('Fizz')
+    })
 });
