0f8db1285621302cec4dba092ba54d7bfd79571b
Alexandre Rocha <alexandrececchin@gmail.com>
2023-06-13 09:47:59 -0300

    [Refactor] Merge test case into test.each

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,26 +1,16 @@
-import {fizzBuzz} from "./fizzbuzz";
+import { BUZZ, FIZZ, FIZZBUZZ, fizzBuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
     it('should return a value type of string',() => {
         expect(typeof fizzBuzz(2)).toBe('string')
     })
 
-    it('should return Fizz when the value is 3',() =>{
-        expect(fizzBuzz(3)).toBe('Fizz')
-    })
-
-    it('should return Buzz when the value is 5',() =>{
-        expect(fizzBuzz(5)).toBe('Buzz')
-    })
-
-
-    it('should return FizzBuzz when the value is 15',() =>{
-        expect(fizzBuzz(15)).toBe('FizzBuzz')
-    })
-
     test.each([
-        {value: 9, mutiple: 3, expected: 'Fizz'},
-        {value: 42, multiple: 3, expected: 'Fizz'},
+        {value: 3, expected: FIZZ},
+        {value: 9, expected: FIZZ},
+        {value: 42, expected: FIZZ},
+        {value: 5, expected: BUZZ},
+        {value: 15, expected: FIZZBUZZ},
     ])('should return $expected when the value is $value', ({value, expected}) => {
         expect(fizzBuzz(value)).toBe(expected)
     });
