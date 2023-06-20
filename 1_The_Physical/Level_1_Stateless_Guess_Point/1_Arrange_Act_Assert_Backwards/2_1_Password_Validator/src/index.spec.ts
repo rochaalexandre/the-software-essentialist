5107c7e1744f55996b64018754faf4d4b744b45b
Alexandre Rocha <alexandrececchin@gmail.com>
2023-06-20 18:51:29 -0300

    [Red] should fail if not contains upper case char

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -39,4 +39,10 @@ describe('password validator', () => {
     expect(result.errors).toHaveLength(1)
   })
 
+  test('Should return an error when the password does not contains any Upper Case Char', () => {
+    const result = PasswordValidator.validate("uak7qeqdubjddir")
+    expect(result.success).toBeFalsy()
+    expect(result.errors).toHaveLength(1)
+  })
+
 })
