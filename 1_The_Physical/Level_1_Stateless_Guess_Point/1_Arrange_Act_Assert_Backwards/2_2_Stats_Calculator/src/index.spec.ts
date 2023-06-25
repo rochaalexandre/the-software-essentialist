702cff2a0cd5760c44375fc72445cfc18fd3a884
Alexandre Rocha <alexandrececchin@gmail.com>
2023-06-25 10:32:27 -0300

    [Refactor] Remove unnecessary test

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,11 +1,6 @@
 import {StatsCaculator} from "./index";
 
 describe('stats calculator', () => {
-    it(' knows how to a create new stats calculator', () => {
-        const calculator = new StatsCaculator();
-        expect(calculator).toBeDefined();
-    })
-
     it(' knows the minimum value in a sequence of integers', () => {
         const numbers = [2, 4, 21, -8, 53, 40];
         expect(StatsCaculator.min(numbers)).toBe(-8);
