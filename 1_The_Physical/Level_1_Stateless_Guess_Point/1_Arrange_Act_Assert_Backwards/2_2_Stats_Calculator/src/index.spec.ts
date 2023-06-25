1dd00063cafe04721beb0a4d906b0f0e0784b07c
Alexandre Rocha <alexandrececchin@gmail.com>
2023-06-25 10:25:35 -0300

    [Green] Create new StatsCalculator

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,14 @@
+import {StatsCaculator} from "./index";
 
 describe('stats calculator', () => {
+    it(' knows how to a create new stats calculator', () => {
+        const calculator = new StatsCaculator();
+        expect(calculator).toBeDefined();
+    })
 
+    it('create new stats calculator instance', () => {
+        const calculator = new StatsCaculator();
+        expect(calculator).toBeDefined();
+    })
 
 })
