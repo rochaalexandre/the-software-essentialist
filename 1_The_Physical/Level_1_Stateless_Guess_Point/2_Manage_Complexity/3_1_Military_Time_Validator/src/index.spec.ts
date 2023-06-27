326548ddb4772a1038dc263df58936694b310ba2
Alexandre Rocha <alexandrececchin@gmail.com>
2023-06-27 18:49:46 -0300

    [Green] knows that 22:00 - 23:12 is a valid time range

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -2,8 +2,9 @@ import {TimeValidator} from "./index";
 
 describe('military time validator', () => {
 
-    it('knows that "01:12 - 14:32" is valid military time range ', () => {
-        expect(TimeValidator.validateRange("01:12 - 14:32")).toBeTruthy()
+    it.each(["22:00 - 23:12","01:12 - 14:32" ])
+    ('knows that "%s" is valid military time range ', (param) => {
+        expect(TimeValidator.validateRange(param)).toBeTruthy()
     });
 
     it('knows that "25:00 - 12:23" is not a valid military time range ', () => {
