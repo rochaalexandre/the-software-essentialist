1b3fe7f87cd9a3daa3c36ed256e6e509256fe063
Alexandre Rocha <alexandrececchin@gmail.com>
2023-06-27 20:26:23 -0300

    [Refactor] Add generic logic to handle time ranges validation

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,5 +1,24 @@
 export class TimeValidator {
+  private static readonly EARLIEST_HOUR: String = "0001";
+  private static readonly LASTEST_HOUR: string = "2359";
   static validateRange(timeRange: string): boolean {
-    return  timeRange !== "25:00 - 12:23"
+    const range = this.getHoursInTimeRange(timeRange)
+    const [startHour, finishHour] = this.convertHoursToNumbers(range)
+
+    if (startHour > finishHour) {
+      return false;
+    }
+
+    return  startHour >= Number(this.EARLIEST_HOUR) && finishHour <= Number(this.LASTEST_HOUR)
+  }
+
+  static getHoursInTimeRange(timeRange: string) {
+    return timeRange.split("-").map((x) => x.trim())
+  }
+
+  static convertHoursToNumbers(range: string[]) {
+    return range
+    .map(x => x.replace(":", "").trim())
+    .map((x) => Number(x));
   }
 }
