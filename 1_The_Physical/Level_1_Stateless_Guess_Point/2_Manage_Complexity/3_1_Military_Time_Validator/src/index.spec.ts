6e685661f45daffe4db9b7a1fe211ce552f99fc3
Alexandre Rocha <alexandrececchin@gmail.com>
2023-06-27 20:26:39 -0300

    [Green] Add extra test

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -2,13 +2,14 @@ import {TimeValidator} from "./index";
 
 describe('military time validator', () => {
 
-    it.each(["22:00 - 23:12","01:12 - 14:32" ])
+    it.each(["22:00 - 23:12","01:12 - 14:32", "06:23 - 07:00", "00:01 - 00:02" ])
     ('knows that "%s" is valid military time range ', (param) => {
         expect(TimeValidator.validateRange(param)).toBeTruthy()
     });
 
-    it('knows that "25:00 - 12:23" is not a valid military time range ', () => {
-        expect(TimeValidator.validateRange("25:00 - 12:23")).toBeFalsy()
+    it.each(["25:00 - 12:23", "15:00 - 14:32", "00:01 - 00:00", "22:00 - 21:12", "15:30 - 25:30"])
+    ('knows that %s is not a valid military time range ', (param) => {
+        expect(TimeValidator.validateRange(param)).toBeFalsy()
     });
 
 })
