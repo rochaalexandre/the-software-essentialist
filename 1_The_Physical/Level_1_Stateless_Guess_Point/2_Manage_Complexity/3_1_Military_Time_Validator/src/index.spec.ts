f2148074e19b91c36d76370d7890295f5a59ca97
Alexandre Rocha <alexandrececchin@gmail.com>
2023-06-27 18:38:18 -0300

    [Green] Start TimeValidator

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,9 @@
+import {TimeValidator} from "./index";
 
 describe('military time validator', () => {
 
+    it('knows how to validate military time ranges', () => {
+        expect(() => TimeValidator.validateRange()).toThrow()
+    });
 
 })
---
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -0,0 +1,5 @@
+export class TimeValidator {
+  static validateRange(): boolean {
+    throw new Error("Method not implemented.");
+  }
+}
