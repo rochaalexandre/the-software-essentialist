6207b80be90127c32dcc04a81d0d9c74df1e1947
Alexandre Rocha <alexandrececchin@gmail.com>
2023-06-27 20:42:15 -0300

    [Refactor] extract code into method and variables with proper name

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -3,19 +3,24 @@ export class TimeValidator {
   private static readonly LASTEST_HOUR: string = "2359";
   private static readonly RANGE_PATTERN = /(\d{2}:\d{2})\s-\s(\d{2}:\d{2})/;
   static validateRange(timeRange: string): boolean {
-    if (!this.RANGE_PATTERN.test(timeRange)) {
+    if (this.isInvalidRange(timeRange)) {
       return false
     }
+
     const range = this.getHoursInTimeRange(timeRange)
     const [startHour, finishHour] = this.convertHoursToNumbers(range)
+    const startHourIsAfterFinishHour = startHour > finishHour;
 
-    if (startHour > finishHour) {
+    if (startHourIsAfterFinishHour) {
       return false;
     }
 
     return  startHour >= Number(this.EARLIEST_HOUR) && finishHour <= Number(this.LASTEST_HOUR)
   }
 
+  static isInvalidRange(range: string) {
+    return !this.RANGE_PATTERN.test(range)
+  }
   static getHoursInTimeRange(timeRange: string) {
     return timeRange.split("-").map((x) => x.trim())
   }
