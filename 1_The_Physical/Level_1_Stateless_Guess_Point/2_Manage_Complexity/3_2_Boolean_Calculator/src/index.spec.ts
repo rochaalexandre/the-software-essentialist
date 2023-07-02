3f1b309e48359077745475d7b163ff4c93ff3601
Alexandre Rocha <alexandrececchin@gmail.com>
2023-07-02 11:27:19 -0300

    [Green] Create BoolanCalculator feature

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,7 @@
+import {BooleanCalculator} from "./index";
 
 describe('boolean calculator', () => {
-
+    it('knows how to calculate boolean expression', () => {
+        expect(BooleanCalculator.evaluate()).toBeFalsy()
+    })
 })
---
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -0,0 +1,5 @@
+export class BooleanCalculator {
+  static evaluate(): any {
+    return false
+  }
+}
